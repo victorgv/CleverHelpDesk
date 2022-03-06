86f9f3acb4835176fd0102ae9c2f5271e9624437
victorgv <victorgv@gmail.com>
2022-03-06 22:02:53 +0100

    spring boot starter project

---
+++ server/src/test/java/es/victorgv/cleverhelpdesk/CleverHelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package es.victorgv.cleverhelpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleverHelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
